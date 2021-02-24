037c9e0f788eec49737238c8677e1f6ecb0d382a
Dan Brooks <danbroooks@users.noreply.github.com>
2021-02-24 19:42:32 +0000

    Intentionally break splitExtras

--- persistent/Database/Persist/Quasi.hs
+++ persistent/Database/Persist/Quasi.hs
@@ -938,17 +938,7 @@ splitExtras
     -> ( [[Text]]
        , M.Map Text [[Text]]
        )
-splitExtras lns =
-    case lns of
-        [] -> ([], M.empty)
-        (Line indent [name]:rest)
-          | not (T.null name) && isUpper (T.head name) ->
-            let (children, rest') = span ((> indent) . lineIndent) rest
-                (x, y) = splitExtras rest'
-             in (x, M.insert name (map tokens children) y)
-        (Line _ ts:rest) ->
-            let (x, y) = splitExtras rest
-             in (ts:x, y)
+splitExtras _ = ([], M.empty)
 
 takeColsEx :: PersistSettings -> [Text] -> Maybe FieldDef
 takeColsEx =
